b5c29e5e565f99ccdd54fcd2e682a22d3f483a2e
Abhishek Bagal <abhibagal34@gmail.com>
2024-11-24 22:19:22 +0530

    first commit
    Make the project Nav and the form header

---
+++ Contact-Form/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './Components/Navigation/Navigation'
+import './App.css'
+import ContactHeader from './Components/ContactHeader/ContactHeader'
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <ContactHeader/>
+    </div>
+  )
+}
+
+export default App
---
+++ Contact-Form/src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import "./Navigation.css"
+const Navigation = () => {
+  return (
+    <div>
+       <nav className='container'>
+        <div className="logo">
+            <img src="/Images/Frame 2 1.png" alt="" />
+        </div>
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>CONTACT</li>
+        </ul>
+        </nav> 
+    </div>
+  )
+}
+
+export default Navigation
